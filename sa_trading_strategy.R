5d3120ab2146857acbf081ed32b63035d8334dc4
Andrew Sun <ajsun12@gmail.com>
2016-11-20 14:37:13 -0500

    R code

--- sa_trading_strategy.R
+++ sa_trading_strategy.R
@@ -1,2 +1,21 @@
-sa_data <- read.csv('out.txt', header = TRUE, stringsAsFactors = FALSE)
+library(timeDate)
+library(dplyr)
+library(quantmod)
+
+
+sa.data <- read.csv('out.txt', header = TRUE, stringsAsFactors = FALSE)
+
+sa.ord <- arrange(sa.data, Date)
+
+stocks = c()
+for (d in 1:nrow(sa.ord)) {
+  stocks.day <- strsplit(sa.ord[d, 2], split = ' ')
+  for (s in stocks.day[[1]]) {
+    if (!(s %in% stocks)) {
+      stocks = append(stocks, s)
+    }
+  }
+}
+
+
 
