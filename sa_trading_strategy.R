d619f523790703beaafcf3dbc1037cb55dbec4b8
Andrew Sun <ajsun12@gmail.com>
2016-11-20 13:16:25 -0500

    adding R code

--- sa_trading_strategy.R
+++ sa_trading_strategy.R
@@ -1,6 +1,2 @@
-sa_data <- read.csv('long_ideas_out.csv', header = TRUE, stringsAsFactors = FALSE)
-
-for (i in 1:nrow(sa_data)) {
-  sa_data[i, 2] <- gsub("[[:punct:]]", "", sa_data[i, 2])
-}
+sa_data <- read.csv('out.txt', header = TRUE, stringsAsFactors = FALSE)
 
